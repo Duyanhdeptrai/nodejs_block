311753203f4a5042f687854a04ca3b2672940d8e
Duy Anh code <95466669+Duyanhdeptrai@users.noreply.github.com>
2024-03-12 00:27:07 +0700

    Duy Anh

--- src/index.js
+++ src/index.js
@@ -6,29 +6,33 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+const router = require('./routes');
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-
+// Middleware
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
 // HTTP LOGGER
 app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', exphbs({
-  extname:'.hbs'
-}));
+app.engine(
+    'hbs',
+    exphbs({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/trangchu', (req, res) => {
-    res.render('home')
-});
-
-app.get('/news', (req, res) => {
-  res.render('news')
-});
+router(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
